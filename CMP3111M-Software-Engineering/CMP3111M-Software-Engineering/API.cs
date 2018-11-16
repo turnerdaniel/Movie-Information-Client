8eb861f09f8229453c84634a1c1afcfdd7abfe2c
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2018-11-16 21:11:01 +0000

    Implemented Search Type logic

--- CMP3111M-Software-Engineering/CMP3111M-Software-Engineering/API.cs
+++ CMP3111M-Software-Engineering/CMP3111M-Software-Engineering/API.cs
@@ -21,7 +21,7 @@ public class Movie // the movie class for containing information about a movie
 {
     public string Poster { get; set; } //image URL (.jpg)
     public string Title { get; set; }
-    public int Year { get; set; }
+    public string Year { get; set; } //string because some dates are ranges
     public string imdbID { get; set; } //format: tt1234567
     public string Type { get; set; }
 }
@@ -39,8 +39,21 @@ public class API
         {
             string jsonString = "";
 
-            jsonString = web.DownloadString("http://www.omdbapi.com/?s=" + "Star Wars" + "&apikey=ffa0df85");
-            
+            switch (searchType)
+            {
+                case "title":
+                    jsonString = web.DownloadString("http://www.omdbapi.com/?s=" + userInput + "&apikey=ffa0df85");
+                    break;
+
+                case "IMDbID":
+                    jsonString = web.DownloadString("http://www.omdbapi.com/?i=" + userInput + "&apikey=ffa0df85");
+                    break;
+
+                default:
+                    //error
+                    break;
+            }
+
             JObject json = JObject.Parse(jsonString);
 
             foreach (var result in json["Search"])
@@ -48,59 +61,12 @@ public class API
                 Movie movie = new Movie();
                 movie.Poster = result["Poster"].ToString();
                 movie.Title = result["Title"].ToString();
-                movie.Year = Int32.Parse(result["Year"].ToString());
+                movie.Year = result["Year"].ToString();
                 movie.imdbID = result["imdbID"].ToString();
                 movie.Type = result["Type"].ToString();
                 results.Add(movie);
             }
         }
-
         return results;
-
-		/*string jsonResult;
-
-		if (searchType == "title") // check whether user has chosen to search by title or ID
-		{
-			jsonResult = Get(@"https://www.omdbapi.com/?s=" + userInput + "&apikey=ffa0df85"); // search by title
-		}
-		else if (searchType == "id")
-		{
-			jsonResult = Get(@"https://www.omdbapi.com/?i=" + userInput + "&apikey=ffa0df8"); // search by ID
-		} else {
-			jsonResult = Get(@"https://www.omdbapi.com/?i=111&apikey=ffa0df8"); // if function is misused this will happen... but it shouldn't
-		}
-		
-		result = JsonConvert.DeserializeObject<List<Movie>>(jsonResult); // convert JSON to list
-		*/
-
-		//XmlDocument xmlResult = new XmlDocument();
-
-		//xmlResult.Load("https://www.omdbapi.com/?s=" + userInput + "&apikey=ffa0df85&r=xml");
-		//XmlNodeList nodes = xmlResult.DocumentElement.SelectNodes("/result");
-
-		//foreach (XmlNode node in nodes)
-		//{
-		//	Movie movie = new Movie();
-		//	movie.Title = node.Attributes["Title"].Value;
-		//	movie.Year = node.Attributes["Year"].Value;
-		//	movie.imdbID = node.Attributes["imdbID"].Value;
-		//	movie.Type = node.Attributes["Type"].Value;
-		//	movie.Poster = node.Attributes["Poster"].Value;
-
-		//	result.Add(movie);
-		//}
-		//return result;
-	}
-
-	public string Get(string uri) // source: https://stackoverflow.com/questions/27108264/c-sharp-how-to-properly-make-a-http-web-get-request
-	{
-		HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-
-		using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-		using (Stream stream = response.GetResponseStream())
-		using (StreamReader reader = new StreamReader(stream))
-		{
-			return reader.ReadToEnd();
-		}
 	}
 }
