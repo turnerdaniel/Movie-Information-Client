716319f5de996988caea6bc05a35f565f45ee69c
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2018-11-16 20:58:35 +0000

    Switched API to JSON

--- CMP3111M-Software-Engineering/CMP3111M-Software-Engineering/API.cs
+++ CMP3111M-Software-Engineering/CMP3111M-Software-Engineering/API.cs
@@ -13,28 +13,57 @@ using System;
 using System.Net;
 using System.IO;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class Movie // the movie class for containing information about a movie
 {
-	public string Title { get; set; }
-	public string Year { get; set; }
-	public string imdbID { get; set; }
-	public string Type { get; set; }
-	public string Poster { get; set; } // this is a URL
+    public string Poster { get; set; } //image URL (.jpg)
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public string imdbID { get; set; } //format: tt1234567
+    public string Type { get; set; }
 }
 
 public class API
 {
-	public API()
-	{
+    //Default Constructor
+	public API() {}
 
-	}
-
-	public List<Movie> getSearch(string searchType, string userInput)
+	public List<Movie> search(string searchType, string userInput)
 	{
-		List<Movie> result = new List<Movie>();
+		List<Movie> results = new List<Movie>();
+
+        using (WebClient web = new WebClient())
+        {
+            string jsonString = web.DownloadString("http://www.omdbapi.com/?s=" + "Star Wars" + "&apikey=ffa0df85");
+
+            JObject json = JObject.Parse(jsonString);
+
+            foreach (var result in json["Search"])
+            {
+                Movie movie = new Movie();
+                movie.Title = result["Title"].ToString();
+                movie.Year = Int32.Parse(result["Year"].ToString());
+                movie.imdbID = result["imdbID"].ToString();
+                movie.Type = result["Type"].ToString();
+                
+
+                //movie.Title = node.Attributes["Title"].Value;
+                //	movie.Year = node.Attributes["Year"].Value;
+                //	movie.imdbID = node.Attributes["imdbID"].Value;
+                //	movie.Type = node.Attributes["Type"].Value;
+                //	movie.Poster = node.Attributes["Poster"].Value;
+
+                results.Add(movie);
+            }
+
+        }
+
+
+        return results;
+
 		/*string jsonResult;
 
 		if (searchType == "title") // check whether user has chosen to search by title or ID
@@ -51,23 +80,23 @@ public class API
 		result = JsonConvert.DeserializeObject<List<Movie>>(jsonResult); // convert JSON to list
 		*/
 
-		XmlDocument xmlResult = new XmlDocument();
+		//XmlDocument xmlResult = new XmlDocument();
 
-		xmlResult.Load("https://www.omdbapi.com/?s=" + userInput + "&apikey=ffa0df85&r=xml");
-		XmlNodeList nodes = xmlResult.DocumentElement.SelectNodes("/result");
+		//xmlResult.Load("https://www.omdbapi.com/?s=" + userInput + "&apikey=ffa0df85&r=xml");
+		//XmlNodeList nodes = xmlResult.DocumentElement.SelectNodes("/result");
 
-		foreach (XmlNode node in nodes)
-		{
-			Movie movie = new Movie();
-			movie.Title = node.Attributes["Title"].Value;
-			movie.Year = node.Attributes["Year"].Value;
-			movie.imdbID = node.Attributes["imdbID"].Value;
-			movie.Type = node.Attributes["Type"].Value;
-			movie.Poster = node.Attributes["Poster"].Value;
+		//foreach (XmlNode node in nodes)
+		//{
+		//	Movie movie = new Movie();
+		//	movie.Title = node.Attributes["Title"].Value;
+		//	movie.Year = node.Attributes["Year"].Value;
+		//	movie.imdbID = node.Attributes["imdbID"].Value;
+		//	movie.Type = node.Attributes["Type"].Value;
+		//	movie.Poster = node.Attributes["Poster"].Value;
 
-			result.Add(movie);
-		}
-		return result;
+		//	result.Add(movie);
+		//}
+		//return result;
 	}
 
 	public string Get(string uri) // source: https://stackoverflow.com/questions/27108264/c-sharp-how-to-properly-make-a-http-web-get-request
